e747ec6e1bf8d9fae9851f73d27a231b1d8a1d50
Barren Wardo <86141456+BarrenWardo@users.noreply.github.com>
2024-04-06 12:41:43 +0530

    pip fix

--- fui.py
+++ fui.py
@@ -28,10 +28,7 @@ fui_image = (
         "git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper.git ./extensions/sd-webui-aspect-ratio-helper",
         "git clone https://github.com/catppuccin/stable-diffusion-webui.git ./extensions/stable-diffusion-webui",
         "git clone https://github.com/adieyal/sd-dynamic-prompts.git ./extensions/sd-dynamic-prompts",
-    )
-    .pip_install_from_requirements(
-        requirements_txt: "/FUI/requirements.txt"
-        gpu: {GPU}
+        "pip install -r requirements.txt"
     )
 )
 
