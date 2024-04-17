2dca14f269ce0b5bc66f858ceee3e0498faf9b71
Barren Wardo <86141456+BarrenWardo@users.noreply.github.com>
2024-04-17 18:32:17 +0530

    Update fooocus.py

--- fooocus.py
+++ fooocus.py
@@ -13,8 +13,6 @@ stub = modal.Stub(
     .run_commands(
         "git clone https://github.com/BarrenWardo/Fooocus",
         "cd /Fooocus",
-        "pip install -r requirements_versions.txt",
-        gpu="t4",
     )
 )
 
@@ -23,7 +21,8 @@ stub = modal.Stub(
         gpu="t4",
         memory=256,
         keep_warm=1,
-        timeout=3600
+        timeout=3600,
+        concurrency_limit=1,
 )
 def run_fooocus():
     fooocus_port = 7860
