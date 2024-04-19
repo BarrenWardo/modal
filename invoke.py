192ee0dc3b4b72fdab43e51803296dde53667694
Barren Wardo <86141456+BarrenWardo@users.noreply.github.com>
2024-04-19 16:42:31 +0530

    updated

--- invoke.py
+++ invoke.py
@@ -1,1 +1,43 @@
+import os
+import subprocess
+import modal
 
+server_timeout = 600
+modal_gpu = "t4"
+DIR = "/root/invokeai"
+
+stub = modal.Stub(
+    image=modal.Image.debian_slim(python_version="3.11")
+    .apt_install(
+        "wget",
+        "git",
+        "libgl1",
+        "libglib2.0-0",
+        "libgl1-mesa-glx",
+        "build-essential",
+        "python3-opencv",
+        "libopencv-dev",
+    )  
+)
+
+volume = modal.Volume.from_name(
+    "invokeai", create_if_missing=True
+)
+
+@stub.function(
+    cpu=4,
+    gpu=modal_gpu,
+    memory=256,
+    keep_warm=1,
+    concurrency_limit=1,
+    volumes={DIR: volume},
+    _allow_background_volume_commits=True,
+)
+
+@web_server(port=9090, startup_timeout=server_timeout)
+
+def run_invokeai():
+    invoke_start = f"""
+    pip install "InvokeAI[xformers]" --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu121 && pip install pypatchmatch && invokeai-web --root {DIR}
+    """
+    subprocess.Popen(invoke_start, shell=True)
