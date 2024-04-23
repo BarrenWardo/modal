3b83345ab3e3ca8aa040e7e5764bbf6e7a62c1b2
Barren Wardo <86141456+BarrenWardo@users.noreply.github.com>
2024-04-23 15:42:21 +0530

    Forge UI with Volume

---
+++ fui-vol.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import modal
+
+modal_gpu = "t4"
+fui_dir = "/root/FUI"
+server_timeout = 1800
+
+app = modal.App(
+    "Forge",
+    image=modal.Image.debian_slim(python_version="3.11")
+    .apt_install(
+        "wget",
+        "git",
+        "libgl1",
+        "libglib2.0-0",
+    )  
+)
+
+volume = modal.Volume.from_name(
+    "FUI", create_if_missing=True
+)
+
+@app.function(
+    cpu=2,
+    gpu=modal_gpu,
+    memory=128,
+    keep_warm=1,
+    concurrency_limit=1,
+    volumes={fui_dir: volume},
+    _allow_background_volume_commits=True,
+    allow_concurrent_inputs=100,
+)
+
+@modal.web_server(port=7860, startup_timeout=server_timeout)
+
+def run_fui():
+    fui_folder = os.path.join(DIR, "FUI")
+    if os.path.exists(fui_folder):
+        fui_start = f"""
+            cd {fui_folder} && git pull && pip install -r requirements.txt && python launch.py --listen --xformers --enable-insecure-extension-access --update-all-extensions
+        """
+    else:
+        fui_start = f"""
+            cd {fui_dir} && git clone https://github.com/BarrenWardo/FUI.git && cd FUI && pip install -r requirements.txt && python launch.py --listen --xformers --enable-insecure-extension-access --update-all-extensions
+        """
+    subprocess.Popen(fui_start, shell=True)
