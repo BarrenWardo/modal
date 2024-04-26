a9848a8306cf0a9dfe92d929a53953178d668237
Barren Wardo <86141456+BarrenWardo@users.noreply.github.com>
2024-04-26 17:09:33 +0530

    Update invoke.py

--- invoke.py
+++ invoke.py
@@ -23,8 +23,7 @@ app = modal.App(
     .pip_install(
         "pypatchmatch",
         "InvokeAI[xformers]",
-        "--use-pep517",
-        "--extra-index-url https://download.pytorch.org/whl/cu121",
+        extra_index_url="https://download.pytorch.org/whl/cu121",
     )
 )
 
