669b603f0c03c3e19e78a816aeb615a6b1fff6a6
barrenwardo <barrenwardo@gmail.com>
2024-05-15 09:58:16 +0000

    idx setup

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,8 +9,6 @@
     # pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
@@ -19,6 +17,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "ms-python.python"
+      "ms-python.debugpy"
     ];
 
     # Enable previews
