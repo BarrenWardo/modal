0cc957bdd67e68a9c3125f676850618e78de9930
Barren Wardo <barrenwardo@gmail.com>
2024-09-21 11:54:26 +0530

    code optimization

--- invoke.py
+++ invoke.py
@@ -39,52 +39,19 @@ app = modal.App(
 def run_invokeai():
     invokeai_port = 9090
     with modal.forward(invokeai_port) as tunnel:
-        # Create the directory
-        mkdir_process = subprocess.Popen(
-            ["mkdir", "-p", "invokeai"],
-            env=os.environ,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        mkdir_stdout, mkdir_stderr = mkdir_process.communicate()
-        if mkdir_process.returncode != 0:
-            print(f"Error creating directory: {mkdir_stderr}")
-            return
+        os.makedirs("invokeai", exist_ok=True)
 
-        # Download the configuration file using wget
-        wget_process = subprocess.Popen(
-            [
-                "wget",
-                "https://gist.githubusercontent.com/BarrenWardo/128c628052d8bc4bea589645bdd4732a/raw/invokeai.yaml",
-                "-O",
-                "invokeai/invokeai.yaml",
-            ],
-            env=os.environ,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        wget_stdout, wget_stderr = wget_process.communicate()
-        if wget_process.returncode != 0:
-            print(f"Error downloading configuration file: {wget_stderr}")
-            return
-
-        # Start the InvokeAI web server
-        invokeai_process = subprocess.Popen(
-            ["invokeai-web"],
-            env=os.environ,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
+        subprocess.run([
+            "wget",
+            "https://gist.githubusercontent.com/BarrenWardo/128c628052d8bc4bea589645bdd4732a/raw/invokeai.yaml",
+            "-O",
+            "invokeai/invokeai.yaml",
+        ], check=True)
 
         print(f"InvokeAI available at => {tunnel.url}")
-        invokeai_process.wait()
+        subprocess.run(["invokeai-web"], check=True)
 
 # Define the local entry point
 @app.local_entrypoint()
 def main():
     run_invokeai.remote()
-#    while True:
-#        pass
